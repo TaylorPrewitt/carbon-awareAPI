ac6d3bb297ac9904d4dbfdfad360c1876883e299
Mohamadou Ly <mohamadouly@microsoft.com>
2021-10-13 11:03:46 -0700

    full path not needed to import DATA_CENTER_INFO_LOCATION

--- Carbon_Aware_API/app/caches/AzureDataCenter.py
+++ Carbon_Aware_API/app/caches/AzureDataCenter.py
@@ -2,7 +2,7 @@ import json
 
 from openpyxl.compat.singleton import Singleton
 
-from Carbon_Aware_API.constant_definitions import DATA_CENTER_INFO_LOCATION
+from constant_definitions import DATA_CENTER_INFO_LOCATION
 
 
 class AzureDataCenterInfo(metaclass=Singleton):
