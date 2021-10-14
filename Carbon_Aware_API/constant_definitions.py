3dd03121a2a113029a66a41c6095e2f245371d11
Nikhitha Musthyala <Nikhitha.Nikhitha@microsoft.com>
2021-10-14 17:39:49 +0530

    Formatting as per pep8 style standards

--- Carbon_Aware_API/constant_definitions.py
+++ Carbon_Aware_API/constant_definitions.py
@@ -1,6 +1,8 @@
 import os
 
-SITE_ROOT = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
+SITE_ROOT = os.path.dirname(os.path.abspath(
+    __file__))  # This is your Project Root
 
 # Data center json data location
-DATA_CENTER_INFO_LOCATION = json_url2 = os.path.join(SITE_ROOT, "app", "static", "az_regions.json")
+DATA_CENTER_INFO_LOCATION = json_url2 = os.path.join(
+    SITE_ROOT, "app", "static", "az_regions.json")
